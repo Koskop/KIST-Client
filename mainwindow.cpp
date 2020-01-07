9b501edb56803c1658148b2c5bbe36cd49dc4cb8
Kostiantyn Kopelets <kostyakopkop@gmail.com>
2020-01-08 00:36:17 +0200

    Finish work with tab 1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,6 @@
 #include "mainwindow.h"
+#include <QJsonArray>
+#include <QStringList>
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
@@ -6,10 +8,156 @@ MainWindow::MainWindow(QWidget *parent)
       , ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
+
+  this->socket = new QTcpSocket(this);
+  connect(socket, SIGNAL(readyRead()), this, SLOT(sockReady()));
+  connect(socket, SIGNAL(disconnected()), this, SLOT(sockDisc()));
+  this->socket->connectToHost("localhost", 4567);
+  this->loadFirstQuery();
 }
 
-MainWindow::~MainWindow()
-{
-  delete ui;
+MainWindow::~MainWindow() { delete ui; }
+
+void MainWindow::sockReady() {
+  if (this->socket->waitForConnected(500)) {
+    this->socket->waitForReadyRead(500);
+    QByteArray arr = this->socket->readAll();
+
+    //    qInfo() << "Test: " << arr;
+
+    QJsonDocument tmp = QJsonDocument::fromBinaryData(arr);
+
+    //    qDebug() << tmp[0].toInt();
+    //    qDebug() << tmp[2][0];
+    //    qDebug() << tmp[2][1];
+    //    qDebug() << arr;
+
+    if (tmp[0].toInt() == 0) {
+      qDebug() << "0";
+      this->requestType0 = tmp;
+      this->processFirstQuery();
+    }
+    if (tmp[0].toInt() == 1) {
+      qDebug() << "1";
+      this->requestType1 = tmp;
+      this->processSecondQuery();
+    }
+    if (tmp[0].toInt() == 2) {
+      qDebug() << "2";
+      this->requestType2 = tmp;
+      this->processThirsQuery();
+    }
+    if (tmp[0].toInt() == 3) {
+      qDebug() << "3";
+      this->requestType3 = tmp;
+      this->processFourthQuery();
+    }
+    if (tmp[0].toInt() == 4) {
+      qDebug() << "4";
+      this->requestType4 = tmp;
+      this->processFivethQuery();
+    }
+    if (tmp[0].toInt() == 5) {
+      qDebug() << "5";
+      this->requestType5 = tmp;
+      this->processSixthQuery();
+    }
+
+    //    qDebug() << arr;
+  }
 }
 
+void MainWindow::loadFirstQuery() {
+  QJsonDocument jsonDoc;
+  QJsonArray jsonMainArray;
+  if (this->socket->waitForConnected(500)) {
+    jsonMainArray.push_back("0");
+    jsonDoc.setArray(jsonMainArray);
+    this->socket->write(jsonDoc.toBinaryData());
+    qDebug() << "write 0";
+  }
+}
+
+void MainWindow::processFirstQuery() {
+  QStringList strL;
+  for (int i = 0; i < this->requestType0[1].toInt(); i++) {
+    strL << this->requestType0[2 + i][1].toString();
+  }
+  this->ui->comboBox->addItems(strL);
+}
+
+void MainWindow::loadSecondQuery() {
+  int personId = -123;
+  QJsonDocument jsonDoc;
+  QJsonArray jsonMainArray;
+  for (int i = 0; i < this->requestType0[1].toInt(); i++) {
+    if (this->requestType0[2 + i][1].toString() ==
+        this->ui->comboBox->currentText()) {
+      personId = this->requestType0[2 + i][0].toString().toInt();
+    }
+  }
+
+  if (this->socket->waitForConnected(500)) {
+    jsonMainArray.push_back("1");
+    jsonMainArray.push_back(QString::number(personId));
+    jsonDoc.setArray(jsonMainArray);
+    this->socket->write(jsonDoc.toBinaryData());
+    qDebug() << "write 1";
+    qDebug() << "Person ID =" << personId;
+  }
+}
+
+void MainWindow::processSecondQuery() {
+  QStandardItemModel *model = new QStandardItemModel;
+  QStandardItem *item;
+
+  //Заголовки столбцов
+  QStringList horizontalHeader;
+  horizontalHeader.append("Violation ID");
+  horizontalHeader.append("Violation kind");
+  horizontalHeader.append("Punish kind");
+  horizontalHeader.append("Order kind");
+
+  model->setHorizontalHeaderLabels(horizontalHeader);
+
+  for (int var = 0; var < requestType1[1].toInt(); ++var) {
+    item = new QStandardItem(requestType1[2 + var][0].toString());
+    model->setItem(var, 0, item);
+    item = new QStandardItem(requestType1[2 + var][1].toString());
+    model->setItem(var, 1, item);
+    item = new QStandardItem(requestType1[2 + var][2].toString());
+    model->setItem(var, 2, item);
+    item = new QStandardItem(requestType1[2 + var][3].toString());
+    model->setItem(var, 3, item);
+  }
+
+  ui->tableView->setModel(model);
+
+  ui->tableView->resizeRowsToContents();
+  ui->tableView->resizeColumnsToContents();
+}
+
+void MainWindow::loadThirsQuery() {
+  if (this->socket->waitForConnected(500)) {
+    this->socket->write("0");
+    qDebug() << "write 0";
+  }
+}
+
+void MainWindow::processThirsQuery() {}
+
+void MainWindow::loadFourthQuery() {}
+
+void MainWindow::processFourthQuery() {}
+
+void MainWindow::loadFivethQuery() {}
+
+void MainWindow::processFivethQuery() {}
+
+void MainWindow::loadSixthQuery() {}
+
+void MainWindow::processSixthQuery() {}
+
+void MainWindow::sockDisc() { this->socket->deleteLater(); }
+
+void MainWindow::on_pushButton_released() { this->loadSecondQuery(); }
--- mainwindow.h
+++ mainwindow.h
@@ -1,10 +1,14 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QByteArray>
+#include <QJsonDocument>
 #include <QMainWindow>
+#include <QTcpSocket>
+#include "QStandardItem"
+#include "QStandardItemModel"
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+QT_BEGIN_NAMESPACE namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -12,10 +16,44 @@ class MainWindow : public QMainWindow
   Q_OBJECT
 
  public:
-  MainWindow(QWidget *parent = nullptr);
+  explicit MainWindow(QWidget *parent = nullptr);
   ~MainWindow();
 
+  QTcpSocket *socket;
+
+  void loadFirstQuery();
+  void processFirstQuery();
+
+  void loadSecondQuery();
+  void processSecondQuery();
+
+  void loadThirsQuery();
+  void processThirsQuery();
+
+  void loadFourthQuery();
+  void processFourthQuery();
+
+  void loadFivethQuery();
+  void processFivethQuery();
+
+  void loadSixthQuery();
+  void processSixthQuery();
+
+ public slots:
+  void sockReady();
+  void sockDisc();
+
+ private slots:
+  void on_pushButton_released();
+
  private:
+  QJsonDocument requestType0;
+  QJsonDocument requestType1;
+  QJsonDocument requestType2;
+  QJsonDocument requestType3;
+  QJsonDocument requestType4;
+  QJsonDocument requestType5;
+
   Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
